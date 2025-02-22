73c7156f85ab29033b9c09ce9dd0399a9735b4d6
mohitfulkar <mohitfulkar@gmail.com>
2025-02-23 02:04:03 +0530

    sidebar fixed in user landing

---
+++ frontend/src/components/user/UserDrawer.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import "../../constants/style.css";
+import { FaRegNewspaper } from "react-icons/fa";
+import { MdOutlineFeedback } from "react-icons/md";
+import { RiBloggerLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
+import { RxCross1 } from "react-icons/rx";
+
+const menuItems = [
+  {
+    label: "Panelists",
+    icon: <FaRegNewspaper />,
+    route: "/panelists",
+  },
+  {
+    label: "News",
+    icon: <FaRegNewspaper />,
+    route: "/news",
+  },
+  {
+    label: "Feedback",
+    icon: <MdOutlineFeedback />,
+    route: "/feedback",
+  },
+  {
+    label: "Blogs",
+    icon: <RiBloggerLine />,
+    route: "/blogs",
+  },
+];
+
+const UserDrawer = ({ isVisible, setIsVisible }) => {
+  return (
+    <div className={`drawer ${isVisible ? "open" : ""}`}>
+      {/* Cross Icon Container */}
+      <div className="flex justify-end p-2">
+        <RxCross1
+          className="cursor-pointer"
+          onClick={() => setIsVisible(false)}
+        />
+      </div>
+
+      {/* Menu Items */}
+      <div className="inner text-center">
+        <ul className="flex flex-col space-y-8">
+          {menuItems.map(({ label, icon, route }) => (
+            <li key={label} className="">
+              <Link to={route} className="menu-item space-x-2 pl-3">
+                {icon}
+                <p className="p">{label}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default UserDrawer;
--- frontend/src/pages/user/UserLanding.jsx
+++ frontend/src/pages/user/UserLanding.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import UserNavbar from "../../components/user/UserNavbar";
+import UserDrawer from "../../components/user/UserDrawer";
+import { RxHamburgerMenu } from "react-icons/rx";
+import "../../constants/style.css";
 
 const UserLanding = () => {
+  const [isVisible, setIsVisible] = useState(true); // Track visibility of UserDrawer
+
+  const handleIsVisible = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <>
       <UserNavbar />
+
+      {/* Hamburger Menu Button */}
+      <div className="relative">
+        <RxHamburgerMenu
+          onClick={handleIsVisible}
+          className="w-[3rem] cursor-pointer z-10 pt-2 h-8" // Hamburger icon stays above everything else
+        />
+
+        {/* Sidebar, position it above the button */}
+        <div className="absolute top-0 left-0 z-20">
+          <UserDrawer isVisible={isVisible} setIsVisible={setIsVisible} />
+        </div>
+      </div>
     </>
   );
 };
