138940d3f04acfee5b430dcbfe2a95f90ddd2061
mohitfulkar <mohitfulkar@gmail.com>
2025-04-21 22:08:53 +0530

    nothing

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import LandingPage from "./LandingPage";
 
 const Landing = () => {
   return (
     <>
       <Navbar />
+      <LandingPage />
     </>
   );
 };
